ba664b392a52d8030e4399c2e10dc7081e89738b
Seudonym <wk170179@gmail.com>
2025-04-19 14:09:37 +0530

    refactor: remove bitflags crate, enums into structs

--- src/main.rs
+++ src/main.rs
@@ -19,4 +19,25 @@ fn main() {
     let mut pca = pca9685::PCA9685::new(i2c, pca9685::Address::default()).unwrap();
 
     let _ = pca.reset(&mut delay).unwrap();
+
+    const SERVO_MIN: u16 = 135;
+    const SERVO_MAX: u16 = 530;
+
+    pca.set_pwm_frequency(50.0).unwrap();
+
+    let mut pulse = SERVO_MIN;
+    loop {
+        pulse = pulse + 1;
+        if pulse == SERVO_MAX {
+            delay.delay_ms(10);
+            pulse = SERVO_MIN;
+            pca.set_pwm(pca9685::Channel::C0, 0, pulse).unwrap();
+            pca.set_pwm(pca9685::Channel::C1, 0, pulse).unwrap();
+            delay.delay_ms(10);
+        }
+        pca.set_pwm(pca9685::Channel::C0, 0, pulse).unwrap();
+        pca.set_pwm(pca9685::Channel::C1, 0, pulse).unwrap();
+        delay.delay_ms(10);
+        log::info!("Pulse: {}", pulse);
+    }
 }
--- src/pca9685.rs
+++ src/pca9685.rs
@@ -7,52 +7,50 @@ pub const PRESCALE_MIN: u8 = 3;
 pub const PRESCALE_MAX: u8 = 255;
 
 /// PCA9685 I2c addresses
-pub enum Register {
-    Mode1 = 0x00,
-    Mode2 = 0x01,
-    SubAddr1 = 0x02,
-    SubAddr2 = 0x03,
-    SubAddr3 = 0x04,
-    AllCallAddr = 0x05,
+pub struct Register;
+impl Register {
+    pub const MODE1: u8 = 0x00;
+    pub const MODE2: u8 = 0x01;
+    pub const SUBADR1: u8 = 0x02;
+    pub const SUBADR2: u8 = 0x03;
+    pub const SUBADR3: u8 = 0x04;
+    pub const ALLCALLADR: u8 = 0x05;
 
-    LED0OnL = 0x06,
-    LED0OnH = 0x07,
-    LED0OffL = 0x08,
-    LED0OffH = 0x09,
+    pub const LED0_ON_L: u8 = 0x06;
+    pub const LED0_ON_H: u8 = 0x07;
+    pub const LED0_OFF_L: u8 = 0x08;
+    pub const LED0_OFF_H: u8 = 0x09;
 
-    // ...
-    AllLEDOnL = 0xFA,
-    AllLEDOnH = 0xFB,
-    AllLEDOffL = 0xFC,
-    AllLEDOffH = 0xFD,
+    pub const ALL_LED_ON_L: u8 = 0xFA;
+    pub const ALL_LED_ON_H: u8 = 0xFB;
+    pub const ALL_LED_OFF_L: u8 = 0xFC;
+    pub const ALL_LED_OFF_H: u8 = 0xFD;
 
-    PreScale = 0xFE,
-    TestMode = 0xFF,
+    pub const PRESCALE: u8 = 0xFE;
+    pub const TEST_MODE: u8 = 0xFF;
 }
 
-bitflags! {
-    /// Mode1 register bits
-    pub struct Mode1: u8 {
-        const ALL_CALL = 0x01;
-        const SUB1 = 0x02;
-        const SUB2 = 0x04;
-        const SUB3 = 0x08;
-        const SLEEP = 0x10;
-        const AUTO_INC = 0x20;
-        const EXT_CLK = 0x40;
-        const RESTART = 0x80;
-    }
+pub struct Mode1;
+/// Mode1 register bits
+impl Mode1 {
+    pub const ALL_CALL: u8 = 0x01;
+    pub const SUB1: u8 = 0x02;
+    pub const SUB2: u8 = 0x04;
+    pub const SUB3: u8 = 0x08;
+    pub const SLEEP: u8 = 0x10;
+    pub const AUTO_INC: u8 = 0x20;
+    pub const EXT_CLK: u8 = 0x40;
+    pub const RESTART: u8 = 0x80;
 }
 
-bitflags! {
+pub struct Mode2;
 /// Mode2 register bits
-    pub struct Mode2: u8 {
-        const OUTNE0 = 0x01;
-        const OUTNE1 = 0x02;
-        const OUTDRV = 0x04;
-        const OCH = 0x08;
-        const INVRT = 0x10;
-    }
+impl Mode2 {
+    pub const OUTNE0: u8 = 0x01;
+    pub const OUTNE1: u8 = 0x02;
+    pub const OUTDRV: u8 = 0x04;
+    pub const OCH: u8 = 0x08;
+    pub const INVRT: u8 = 0x10;
 }
 
 /// Output channel selection
@@ -140,7 +138,7 @@ where
         D: DelayNs,
     {
         // Reset the PCA9685 by writing 0x00 to the mode1 register
-        self.write_register(Register::Mode1, Mode1::RESTART.bits())?;
+        self.write_register(Register::MODE1, Mode1::RESTART)?;
         delay.delay_ms(5);
         Ok(())
     }
@@ -148,18 +146,66 @@ where
     /// Initialize the PCA9685
     fn init(&mut self) -> Result<(), E> {
         // Set the mode1 register to 0x00
-        self.write_register(Register::Mode1, 0x01)?;
+        self.write_register(Register::MODE1, 0x01)?;
         Ok(())
     }
 
+    /// Use low power mode
+    pub fn sleep(&mut self) -> Result<(), E> {
+        let mode1 = self.read_register(Register::MODE1)?;
+        self.write_register(Register::MODE1, mode1 | Mode1::SLEEP)
+    }
+
+    /// Wake up from low power mode
+    pub fn wake(&mut self) -> Result<(), E> {
+        let mode1 = self.read_register(Register::MODE1)?;
+        self.write_register(Register::MODE1, mode1 & !Mode1::SLEEP)
+    }
+
+    /// Set the PWM frequency
+    /// The formula for prescale is given by:
+    ///      prescale = round(osc_clock / (4096 * frequency)) - 1
+    pub fn set_pwm_frequency(&mut self, frequency: f32) -> Result<(), E> {
+        let prescale = (25_000_000.0 / (4096.0 * frequency) - 1.0).round() as u8;
+        self.sleep()?;
+        self.write_register(Register::PRESCALE, prescale)?;
+        self.wake()
+    }
+
+    ///
+    pub fn set_pwm(&mut self, channel: Channel, on: u16, off: u16) -> Result<(), E> {
+        let channel = match channel {
+            Channel::C0 => Register::LED0_ON_L,
+            Channel::C1 => Register::LED0_ON_L + 4,
+            Channel::C2 => Register::LED0_ON_L + 8,
+            Channel::C3 => Register::LED0_ON_L + 12,
+            Channel::C4 => Register::LED0_ON_L + 16,
+            Channel::C5 => Register::LED0_ON_L + 20,
+            Channel::C6 => Register::LED0_ON_L + 24,
+            Channel::C7 => Register::LED0_ON_L + 28,
+            Channel::C8 => Register::LED0_ON_L + 32,
+            Channel::C9 => Register::LED0_ON_L + 36,
+            Channel::C10 => Register::LED0_ON_L + 40,
+            Channel::C11 => Register::LED0_ON_L + 44,
+            Channel::C12 => Register::LED0_ON_L + 48,
+            Channel::C13 => Register::LED0_ON_L + 52,
+            Channel::C14 => Register::LED0_ON_L + 56,
+            Channel::C15 => Register::LED0_ON_L + 60,
+        };
+        self.write_register(channel, on as u8)?;
+        self.write_register(channel + 1, (on >> 8) as u8)?;
+        self.write_register(channel + 2, off as u8)?;
+        self.write_register(channel + 3, (off >> 8) as u8)
+    }
+
     /// Write a byte to a register
-    pub fn write_register(&mut self, reg: Register, value: u8) -> Result<(), E> {
+    pub fn write_register(&mut self, reg: u8, value: u8) -> Result<(), E> {
         let mut buf = [reg as u8, value];
         self.i2c.write(self.address, &buf)
     }
 
     /// Read a byte from a register
-    pub fn read_register(&mut self, reg: Register) -> Result<u8, E> {
+    pub fn read_register(&mut self, reg: u8) -> Result<u8, E> {
         let mut buf = [0; 1];
         self.i2c.write_read(self.address, &[reg as u8], &mut buf)?;
         Ok(buf[0])
